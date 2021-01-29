df637fafafca6876f7e1915cd3ee9b4c2fc799eb
Dinga <kefundinga@yahoo.com>
2021-01-28 21:08:41 -0700

    upgrade

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('we are on business!'));
+app.get('/', (req, res) => res.send('we are on business only!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
